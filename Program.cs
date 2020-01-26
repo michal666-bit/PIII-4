051bb3c1ef6da6e832242b43772243d07c750c4a
michal666-bit <gajewski.michal88@gmail.com>
2020-01-26 12:43:04 +0100

    PIII-4

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ namespace Lab4
 {
     class Program
     {
-       static int [] Kopiuj(int[] tablica)
+       static int [] Kopiuj( ref int[] tablica)
         {
             int[] nowa = new int [tablica.Length];
             for (int i = 0; i < nowa.Length; i++)
             {
                nowa[i] = tablica[i];
             }
-            return nowa[];
+            return nowa;
         }
         static void Main(string[] args)
         {
@@ -51,25 +51,68 @@ namespace Lab4
                 new[]{1,2,3},
                 new[]{1,2,3,4},
                 new[]{1,2,3,4,5},
-                }; 
-            for (int i = 0; i < y.Length; i++)
+                };
+
+            Console.WriteLine("Przed kopiowaniem:");
+
+            int[] x1 = new int[4] { 1, 2, 3, 4 };
+            int[] x2 = new int[2] { 5, 6 };
+            int[] x3 = new int[3] { 1, 2, 2 };
+            int[] x4 = new int[4] { 5, 4, 3, 2 };
+            int[][] z = new int[][] { x1, x2, x3, x4 };
+
+            for (int i = 0; i < x.GetLength(0); i++)
             {
-                
+                for (int j = 0; j < z[i].Length; j++)
+                {
+                    Console.Write(z[i][j] + " ");
+                }
+                Console.WriteLine();
             }
-            int[] skopowiana = Kopiuj(y);
-            
-        //    for (int i = 0; i < y.GetLength(0); i++)
-        //    {
-        //        for (int j = 0; j < y[i].GetLength(0); j++)
-        //        {
-        //            Console.Write(y[i][j]);
-        //        }
-        //       Console.WriteLine();
-        //    }
-        //    Console.WriteLine();
+            Console.WriteLine();
+            //int[][] z = new int[5][];
+            //for (int i = 0; i < z.Length; i++)
+            //{
+            //    z[i] = new int[i + 1];
+            //    for (int j = 0; j < z[i].Length; j++)
+            //    {
+            //        z[i][j] = i + j;
+
+            //    }
+            //}
+
+
+            Kopiuj(ref x1);
+            Console.WriteLine("Po kopiowaniu:");
+            //for (int i = 0; i < z.Length; i++)
+            //{
+            //    for (int j = 0; j < z[i].Length; j++)
+            //    {
+            //        Console.Write(z[i][j] + ", ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            /*
+            for (int i = 0; i < y.GetLength(0); i++)
+            {
+                for (int j = 0; j < y.GetLength(1); j++)
+                {
+                    Console.Write(y[i, j]);
+                }
+                Console.WriteLine();
+            }
+            */
+
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < z[i].Length; j++)
+                {
+                    Console.Write(z[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
-
-
-
